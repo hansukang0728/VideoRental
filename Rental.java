0b2a295d5e6accf19193674c18a8b951cc5acda6
kmn.kang <jara1240@gmail.com>
2022-09-07 16:52:10 +0900

    Duplicate Code 제거 Customer.java, Rental.java

--- Rental.java
+++ Rental.java
@@ -41,13 +41,7 @@ public class Rental {
 	public int getDaysRentedLimit() {
 		int limit = 0 ;
 		int daysRented ;
-		if (getStatus() == 1) { // returned Video
-			long diff = returnDate.getTime() - rentDate.getTime();
-			daysRented = (int) (diff / (1000 * 60 * 60 * 24)) + 1;
-		} else { // not yet returned
-			long diff = new Date().getTime() - rentDate.getTime();
-			daysRented = (int) (diff / (1000 * 60 * 60 * 24)) + 1;
-		}
+		daysRented = getDaysRented();
 		if ( daysRented <= 2) return limit ;
 
 		switch ( video.getVideoType() ) {
@@ -56,5 +50,17 @@ public class Rental {
 			case Video.DVD: limit = 2 ; break ;
 		}
 		return limit ;
+	}
+
+	public int getDaysRented() {
+		int daysRented;
+		if (getStatus() == 1) { // returned Video
+			long diff = returnDate.getTime() - rentDate.getTime();
+			daysRented = (int) (diff / (1000 * 60 * 60 * 24)) + 1;
+		} else { // not yet returned
+			long diff = new Date().getTime() - rentDate.getTime();
+			daysRented = (int) (diff / (1000 * 60 * 60 * 24)) + 1;
+		}
+		return daysRented;
 	}
 }
